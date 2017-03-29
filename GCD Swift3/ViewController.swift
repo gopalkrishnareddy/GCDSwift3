ed283ae2433f3dcd77823958c4b461adb82e0fde
Yoel Lev <yoelev@yoels-mac-pro.local>
2017-03-29 14:58:26 +0300

    Added : • Concurrent Queues • Quality Of Service (QoS) and Priorities

--- GCD Swift3/ViewController.swift
+++ GCD Swift3/ViewController.swift
@@ -21,25 +21,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         // Sync and Async Queues
         
-        simpleSyncQueues()
+        //simpleSyncQueues()
         
-        simpleAsyncQueues()
+        //simpleAsyncQueues()
        
 
         
-        // queuesWithQoS()
+       //  queuesWithQoS()
         
-        /*
+        
          concurrentQueues()
          if let queue = inactiveQueue {
          queue.activate()
+            
+            
          }
-         */
+        
         
         // queueWithDelay()
         
@@ -71,9 +75,7 @@ class ViewController: UIViewController {
  
          print("##### END Synchronous Execution ##### ", "\n")
     }
-    
-    
-    
+
     func simpleAsyncQueues(){
         
          print("##### Asynchronous Execution ##### ")
@@ -98,18 +100,106 @@ class ViewController: UIViewController {
     
     
     
+    /*
+     # Quality Of Service  Priority Table #
+     
+     • userInteractive (Highest)
+     • userInitiated
+     • default
+     • utility
+     • background
+     • unspecified (Lowest)
+     
+     */
+    
+    
+    //Quality Of Service (QoS) and Priorities
     func queuesWithQoS() {
         
-    }
-    
-    
-    var inactiveQueue: DispatchQueue!
-    func concurrentQueues() {
+         print("##### Quality Of Service Execution ##### ", "\n")
+        
+        let queue1 = DispatchQueue(label: "com.yoelev.queue1", qos: DispatchQoS.userInitiated)
+       // let queue2 = DispatchQueue(label: "com.yoelev.queue2", qos: DispatchQoS.userInitiated)
+        
+     //   let queue1 = DispatchQueue(label: "com.yoelev.queue1", qos: DispatchQoS.background)
+        let queue2 = DispatchQueue(label: "com.yoelev.queue2", qos: DispatchQoS.utility)
+        
+        
+        queue1.async {
+            for i in 0..<10 {
+                print("🍎",i)
+                
+            }
+        }
+        
+        
+        queue2.async {
+            for i in 100..<110 {
+                print("🔵",i)
+                
+            }
+        }
+        
+        
+        
+        for i in 1000..<1010 {
+            print("Ⓜ️", i)
+        }
+        
+     
+         print("##### END Quality Of Service Execution ##### ", "\n")
         
     }
     
     
+    
+    
+    //Concurrent Queues 
+    var inactiveQueue: DispatchQueue!
+    
+    func concurrentQueues() {
+        
+        print("##### Concurrent Queues Execution ##### ", "\n")
+        
+//        let anotherQueue = DispatchQueue(label: "com.yoelev.anotherQueue", qos: .utility)
+//        let anotherQueue = DispatchQueue(label: "com.yoelev.anotherQueue", qos: .utility, attributes: .concurrent)
+        
+    let anotherQueue = DispatchQueue(label: "com.yoelev.anotherQueue", qos: .userInitiated, attributes: [.concurrent, .initiallyInactive])
+        inactiveQueue = anotherQueue
+        
+
+        anotherQueue.async {
+            for i in 0..<10 {
+                print("🔴",i)
+                
+            }
+        }
+        
+        anotherQueue.async {
+            for i in 100..<110 {
+                print("🔵",i)
+                
+            }
+        }
+        
+        anotherQueue.async {
+            for i in 100..<110 {
+                print("⚫️",i)
+                
+            }
+        }
+        
+        anotherQueue.async {
+            print("##### End Concurrent Queues Execution ##### ", "\n")
+
+        }
+        
+    }
+    
+    //Delaying the Execution
     func queueWithDelay() {
+        
+        
         
     }
     
